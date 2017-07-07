cec4965b6406cccadd8061e3e15506dbf2334ca7
Tao Wen <taowen@gmail.com>
2017-07-07 13:40:57 +0800

    add object copier

--- util/copy.go
+++ util/copy.go
@@ -8,16 +8,34 @@ import (
 	"unsafe"
 )
 
+var ObjectCopierProviders = []func(dstType, srcType reflect.Type) (ObjectCopier, error){}
 var CopierProviders = []func(dstAccessor, srcAccessor lang.Accessor) (Copier, error){}
 
 func Copy(dst, src interface{}) error {
-	dstAccessor := lang.AccessorOf(reflect.TypeOf(dst))
-	srcAccessor := lang.AccessorOf(reflect.TypeOf(src))
-	copier, err := CopierOf(dstAccessor, srcAccessor)
+	copier, err := ObjectCopierOf(reflect.TypeOf(dst), reflect.TypeOf(src))
 	if err != nil {
 		return err
 	}
-	return copier.Copy(lang.AddressOf(dst), lang.AddressOf(src))
+	return copier.Copy(dst, src)
+}
+
+func ObjectCopierOf(dstType, srcType reflect.Type) (ObjectCopier, error) {
+	for _, provider := range ObjectCopierProviders {
+		copier, err := provider(dstType, srcType)
+		if err != nil {
+			return nil, err
+		}
+		if copier != nil {
+			return copier, err
+		}
+	}
+	dstAccessor := lang.AccessorOf(dstType)
+	srcAccessor := lang.AccessorOf(srcType)
+	copier, err := CopierOf(dstAccessor, srcAccessor)
+	if err != nil {
+		return nil, err
+	}
+	return &defaultObjectCopier{copier}, nil
 }
 
 func CopierOf(dstAccessor, srcAccessor lang.Accessor) (Copier, error) {
@@ -33,6 +51,18 @@ func CopierOf(dstAccessor, srcAccessor lang.Accessor) (Copier, error) {
 	return nil, errors.New(fmt.Sprintf("no copier for %#v => %#v", srcAccessor, dstAccessor))
 }
 
+type ObjectCopier interface {
+	Copy(dst interface{}, src interface{}) error
+}
+
 type Copier interface {
 	Copy(dst unsafe.Pointer, src unsafe.Pointer) error
+}
+
+type defaultObjectCopier struct {
+	copier Copier
+}
+
+func (objCopier *defaultObjectCopier) Copy(dst interface{}, src interface{}) error {
+	return objCopier.copier.Copy(lang.AddressOf(dst), lang.AddressOf(src))
 }
