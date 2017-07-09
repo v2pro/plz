68668da77d13e5f07cda6ec1337daf6928a8a016
Tao Wen <taowen@gmail.com>
2017-07-09 22:31:30 +0800

    update comment

--- util/functional.go
+++ util/functional.go
@@ -1,8 +1,15 @@
 package util
 
+// Max takes ints/floats as input. or only one array/slice as input.
+// When input is only one array/slice, the element can be interface{}/ints/floats/pointers to numbers.
+// Arguments must be same type. The return type will keep original element type.
 var Max = func(collection ...interface{}) interface{} {
 	panic("not implemented")
 }
+
+// Min takes ints/floats as input. or only one array/slice as input.
+// When input is only one array/slice, the element can be interface{}/ints/floats/pointers to numbers.
+// Arguments must be same type. The return type will keep original element type.
 var Min = func(collection ...interface{}) interface{} {
 	panic("not implemented")
 }
