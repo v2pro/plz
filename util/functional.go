95719f6763742107e9540730aeb03f669f8a7b5d
Tao Wen <taowen@gmail.com>
2017-07-11 13:06:53 +0800

    redesign plz.Max

--- util/functional.go
+++ util/functional.go
@@ -1,9 +1,52 @@
 package util
 
+import "reflect"
+
+var maxSimpleValue = map[reflect.Type]func(collection []interface{}) interface{}{}
+var GenMaxSimpleValue = func(typ reflect.Type) func(collection []interface{}) interface{} {
+	panic("not implemented")
+}
+
+type structAndField struct {
+	S reflect.Type
+	F string
+}
+
+var maxStructByField = map[structAndField]func(collection []interface{}) interface{}{}
+var GenMaxStructByField = func(typ reflect.Type, fieldName string) func(collection []interface{}) interface{} {
+	panic("not implemented")
+}
+
 // Max takes ints/floats as input. or only one array/slice as input.
 // When input is only one array/slice, the element can be interface{}/ints/floats/pointers to numbers.
 // Arguments must be same type. The return type will keep original element type.
-var Max = func(collection ...interface{}) interface{} {
+func Max(collection ...interface{}) interface{} {
+	if len(collection) == 0 {
+		return nil
+	}
+	typ := reflect.TypeOf(collection[0])
+	f := maxSimpleValue[typ]
+	if f != nil {
+		return f(collection)
+	}
+	f = GenMaxSimpleValue(typ)
+	if f != nil {
+		maxSimpleValue[typ] = f
+		return f(collection)
+	}
+	lastElem, isString := collection[len(collection)-1].(string)
+	if isString {
+		cacheKey := structAndField{typ, lastElem}
+		f = maxStructByField[cacheKey]
+		if f != nil {
+			return f(collection[:len(collection)-1])
+		}
+		f = GenMaxStructByField(typ, lastElem)
+		if f != nil {
+			maxStructByField[cacheKey] = f
+			return f(collection[:len(collection)-1])
+		}
+	}
 	panic("not implemented")
 }
 
