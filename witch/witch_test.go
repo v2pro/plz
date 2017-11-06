cbecb9e0ed0376e30328987c2a3c0dd46f60093a
Tao Wen <taowen@gmail.com>
2017-11-06 15:18:15 +0800

    cap length

--- witch/witch_test.go
+++ witch/witch_test.go
@@ -11,8 +11,12 @@ func Test_witch(t *testing.T) {
 	fakeValues := []string{"tom", "jerry", "william", "lily"}
 	go func() {
 		for {
+			response := []byte{}
+			for i := int32(0); i < rand.Int31n(1024 * 256); i++ {
+				response = append(response, fakeValues[rand.Int31n(4)]...)
+			}
 			countlog.Info("event!hello", "user", fakeValues[rand.Int31n(4)],
-				"response", "!!!!!!!!!")
+				"response", string(response))
 			time.Sleep(time.Millisecond * 50)
 		}
 	}()
