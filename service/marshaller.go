593bfaee4dcedcc31b128d9fd29cc81655eae902
Tao Wen <taowen@gmail.com>
2018-01-28 22:38:36 +0800

    remove concrete impl from service

--- service/marshaller.go
+++ service/marshaller.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"github.com/json-iterator/go"
 	"github.com/v2pro/plz/countlog"
 )
 
@@ -12,64 +11,4 @@ type Response struct {
 
 type Marshaller interface {
 	Marshal(ctx *countlog.Context, output interface{}, obj interface{}) error
-}
-
-type jsoniterResponseMarshaller struct {
-}
-
-
-func NewJsoniterResponseMarshaller() Marshaller {
-	return &jsoniterResponseMarshaller{}
-}
-
-func (marshaller *jsoniterResponseMarshaller) Marshal(ctx *countlog.Context, output interface{}, obj interface{}) error {
-	stream := jsoniter.ConfigDefault.BorrowStream(nil)
-	defer jsoniter.ConfigDefault.ReturnStream(stream)
-	stream.WriteObjectStart()
-	stream.WriteObjectField("errno")
-	resp := obj.(Response)
-	if resp.Error != nil {
-		errno, _ := resp.Error.(errNo)
-		if errno == nil {
-			stream.WriteInt(1)
-		} else {
-			stream.WriteInt(errno.ErrorNumber())
-		}
-		stream.WriteMore()
-		stream.WriteObjectField("errmsg")
-		stream.WriteString(resp.Error.Error())
-	} else {
-		stream.WriteInt(0)
-	}
-	stream.WriteMore()
-	stream.WriteObjectField("data")
-	stream.WriteVal(resp.Object)
-	stream.WriteObjectEnd()
-	if stream.Error != nil {
-		return stream.Error
-	}
-	ptrBuf := output.(*[]byte)
-	*ptrBuf = append(([]byte)(nil), stream.Buffer()...)
-	return nil
-}
-
-type errNo interface {
-	ErrorNumber() int
-}
-
-type jsoniterMarshaller struct {
-}
-
-func NewJsoniterMarshaller() Marshaller {
-	return &jsoniterMarshaller{}
-}
-
-func (marshaller *jsoniterMarshaller) Marshal(ctx *countlog.Context, output interface{}, obj interface{}) error {
-	buf, err := jsoniter.Marshal(obj)
-	if err != nil {
-		return err
-	}
-	ptrBuf := output.(*[]byte)
-	*ptrBuf = buf
-	return nil
 }
--- service/unmarshaller.go
+++ service/unmarshaller.go
@@ -1,22 +1,9 @@
 package service
 
 import (
-	"github.com/json-iterator/go"
 	"github.com/v2pro/plz/countlog"
 )
 
 type Unmarshaller interface {
 	Unmarshal(ctx *countlog.Context, obj interface{}, input interface{}) error
-}
-
-type jsoniterUnmarshaller struct {
-	protoInterface emptyInterface
-}
-
-func NewJsoniterUnmarshaller() Unmarshaller {
-	return &jsoniterUnmarshaller{}
-}
-
-func (unmarshaller *jsoniterUnmarshaller) Unmarshal(ctx *countlog.Context, obj interface{}, input interface{}) error {
-	return jsoniter.Unmarshal(input.([]byte), obj)
 }
